04d338218ea7eec2c96ac2200ccf5ee681a2a06c
Nathasha_Dewduni <ndewduni12@gmail.com>
2026-03-11 11:50:52 +0530

    update product api

--- ecommerce-platform/server/routes/productRoutes.js
+++ ecommerce-platform/server/routes/productRoutes.js
@@ -30,7 +30,63 @@ router.get('/', (req, res) => {
 });
 
 
+//GET SINGLE PRODUCT BY ID
+router.get('/:id',(req,res)=> {
+    const productId =req.params.id;
+
+    const sql = "SELECT * FROM products WHERE id = ?";
+    db.query(sql ,[productId], (err,results) => {
+      if (err )
+        return res.status(500).json ({ error: err.message});
+
+      if(results.length===0){
+        return res.status(404).json({ message: "Product not found"});
+    
+      }
+      res.json(results[0]);
+  })
+});
 
 
+//UPDATE PRODUCT
+router.put('/update/:id', (req, res) => {
+    const productId = req.params.id;
+    const {name, description, price, stock, image_url } = req.body;
+
+    const sql = `
+        UPDATE products
+        SET name =?, description =?, price =?, stock =?, image_url =?
+        WHERE id =?
+        `;
+    
+    db.query(sql,[ name, description, price, stock, image_url, productId], (err, result) =>{
+        if(err)
+            return res.status(500).json( {error:err.message});
+
+        if(result.affectedRows===0){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.json({message: "Product updated successfully"});
+    });
+
+});
+
+//DELETE PRODUCT
+router.delete('/delete/:id', (req,res) =>{
+    const productId =req.params.body;
+    
+    const sql = "SELECT * FROM products WHERE id= ?";
+
+    db.query(sql, [productId], (err,result) =>{
+        if(err)
+            return res.status(500).json({error:err.message});
+
+        if(result.affectedRows===0 ){
+            return res.status(404).json({message:"Product not found"});
+        }
+        res.json({message: " Product deleted successfully"})
+    });
+
+});
 
 module.exports = router;
